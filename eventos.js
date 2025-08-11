5baac94162bcf43ba4eb969241d9cf5944f8f6c1
María Cavallo <mariacavallom@gmail.com>
2025-08-11 14:00:46 -0300

    Agregado eventos.js y manejadores de click

---
+++ eventos.js
@@ -0,0 +1,7 @@
+document.getElementById('contenedor').addEventListener('click', function() {
+    alert('Hola! Soy el div');
+});
+
+document.querySelector('button').addEventListener('click', function(event) {
+    alert('Hola!');
+});
